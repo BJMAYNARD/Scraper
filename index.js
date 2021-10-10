b7b96f79ebb18ed7c34d94474078ae29a68d73e3
BJ Maynard <linuxmaynard@gmail.com>
2021-10-09 21:28:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const PORT = 8989
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { append, existsOne } = require('domutils')
+const express = require('express')
+const app = express()
+
+
+app.listen(PORT,() => console.log)
+
+const url ='https://www.theguardian.com/us-news'
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+    
+       $('.fc-item__title', html).each(function() {
+        const Title = $(this).text()
+        const URL = $(this).find("a").attr('href')
+        articles.push({
+            Title,
+            URL
+        })
+       })
+    
+       console.log(articles)
+    }).catch(err => console.log(err))
+;
+
