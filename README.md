d64a28991204f055328113aabe6de13773d5f868
BJ Maynard <linuxmaynard@gmail.com>
2021-10-10 16:45:22 -0400

    Changes
    Made it so it would create the txt file

--- README.md
+++ README.md
@@ -7,8 +7,5 @@ In Terminal: npm run scrape
 This will get the Title and URL of Headlines from 'https://www.theguardian.com/us-news' using the H2 class tag.
 I have tried several other websites with success. Matching the class tags is where it can get tricky.
 
-Once you run the scrape - wait about 10 seconds and hit "CNTRL + C" and then run the command "node index.js > index.log 2>&1" to get a log 
-of clickable URL sites with the headlines.
-
-
-Coming Soon: Having the commands built into the code.
+I got the original idea from 'Code with Ania Kubów' on YouTube and expanded the idea. This version created a txt file within the main 
+files that you can just open up and check out todays headlines.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//Needed Constants
 const PORT = 8989
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -6,19 +6,23 @@ const express = require('express')
 const app = express()
 
 
+//Tells the console.log to listen to desird port
 app.listen(PORT,() => console.log)
 
+//This is where you do the URL
 const url ='https://www.theguardian.com/us-news'
 axios(url)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
         const articles = []
+        //Named the array 
 
-    
+//Find the class tag of a given headline that works with what you are trying to get    
        $('.fc-item__title', html).each(function() {
         const Title = $(this).text()
         const URL = $(this).find("a").attr('href')
+        //We push the console.log to the array
         articles.push({
             Title,
             URL
@@ -29,3 +33,22 @@ axios(url)
     }).catch(err => console.log(err))
 ;
 
+//Sometimes it takes  more than a second to get the needed files
+setTimeout((function() {  
+    return process.exit();
+}), 15000);
+
+
+//Now we tell the node to change the console.log to be redirected to a new file that is created.
+//In this case we named it console_log.txt
+
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/console_log.txt', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+console.log('Go Check Out The console_log.txt Files For Clickable Links To Todays Headlines');
